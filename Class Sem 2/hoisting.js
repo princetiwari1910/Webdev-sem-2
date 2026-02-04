8b834bf1e154237b7a579227b65d35d242ba1fd2
Prince Tiwari <princetiwari19october@gmail.com>
2026-02-04 10:51:14 +0530

    restore Class Sem 2 folder

---
+++ Class Sem 2/hoisting.js
@@ -0,0 +1,6 @@
+var add=function(a,b){
+    return a+b;
+}
+var a=add(3,4);
+console.log(a);
+
---
+++ Class Sem 2/ifelse.js
@@ -0,0 +1,18 @@
+var myscore= 92;
+if(myscore>90){
+    console.log("Yayy got my bicyvle");
+}
+else{
+    console.log('Oops did not get this time');
+}
+
+var studentscore=95;
+if(studentscore>=90){
+    console.log('A');
+}
+else if(studentscore>=70 && studentscore<=89){
+    console.log('B');
+}
+else{
+    console.log('C');
+}
